80dc4a8d457009f08ddb0d17fe9a8abe2e7caf93
Gavin Loughridge <gavinlough@gmail.com>
2017-11-23 13:36:33 -0600

    Initial Commit

---
+++ intentions/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  intentions
+//
+//  Created by Gavin Loughridge on 11/23/17.
+//  Copyright © 2017 Gavin Loughridge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
